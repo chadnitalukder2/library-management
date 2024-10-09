c0577af7d5f34aafc81bf52ef5026ad018be4212
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-10-09 23:49:01 +0600

    wip::borrow book

--- includes/Views/Books/BorrowBook.php
+++ includes/Views/Books/BorrowBook.php
@@ -1,5 +1,5 @@
 <?php
-
+ $image = LIBRARYMANAGEMENT_URL . 'assets/images/sunflower.jpg';
 ?>
 <div class="lmt_borrow_books">
     <div class="lmt_borrow_header">
@@ -10,15 +10,19 @@
             <form id="lmt_submission-borrow-form">
                 <div class="lmt_borrow_form">
                 <div class="lmt_form_group">
-                        <input type="number" name="name" id="">
+                        <input type="hidden"  name="name" id="">
                     </div>
                     <div class="lmt_form_group">
-                        <p for="name">Start Date</p>
-                        <input type="date" name="name" id="lmt_name" required="">
+                        <p for="name">Borrow Date</p>
+                        <input type="date" class="lmt_input" name="name" id="lmt_name" required="">
                     </div>
                     <div class="lmt_form_group">
-                        <p for="name">End Date</p>
-                        <input type="date" name="name" id="lmt_name" required="">
+                        <p for="name">Return Date</p>
+                        <input type="date" class="lmt_input" name="name" id="lmt_name" required="">
+                    </div>
+
+                    <div class="lmt_form_group">
+                        <button class="lmt_borrow_btn">Borrow Book</button>
                     </div>
                 </div>
             </form>
@@ -26,7 +30,60 @@
         </div>
         <!-- ============================= -->
         <div class="lmt_book_details">
-
+            <div class="lmt_book_name">
+                <h1>Science book</h1>
+            </div>
+            <div class="lmt_book_image">
+                <img src="<?php echo $image ?>">
+            </div>
+            <div class="lmt_borrow_book_details">
+                <div class="lmt_left">
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons dashicons-category"></span>
+                        </div>
+                        <span class="lmt_name"> <span >Category : </span><?php echo esc_html($category_name) ?></span>
+                    </div>
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons dashicons-admin-users"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Author : </span><?php echo esc_html($author) ?></span>
+                    </div>
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons dashicons-admin-users"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Publisher : </span><?php echo esc_html($author) ?></span>
+                    </div>
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons dashicons-admin-users"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Published Date : </span><?php echo esc_html($author) ?></span>
+                    </div>
+                </div>
+                <div class="lmt_right">
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons dashicons-edit-large"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Edition : </span><?php echo esc_html($edition) ?></span>
+                    </div>
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons  dashicons-analytics"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Quantity : </span><?php echo esc_html($quantity) ?></span>
+                    </div>
+                    <div class="lmt_book">
+                        <div class="lmt_icon_box">
+                            <span class="icon dashicons  dashicons-analytics"></span>
+                        </div>
+                        <span class="lmt_name"> <span>Added Date : </span><?php echo esc_html($quantity) ?></span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
    
